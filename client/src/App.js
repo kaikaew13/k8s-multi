3723be8fafb887d98044424fc03d35dc1a66181b
kaikaew13 <ksaratunti@gmail.com>
2021-06-09 13:19:47 +0700

    change h1 tag

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
